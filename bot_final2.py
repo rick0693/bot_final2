25177d8fe67f4917fb467f31c6763f8cb1555502
rick0693 <108191105+rick0693@users.noreply.github.com>
2023-08-23 23:09:14 -0300

    Update bot_final2.py

--- bot_final2.py
+++ bot_final2.py
@@ -9,6 +9,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 st.set_page_config(page_title=None, page_icon="🚚", layout="wide", initial_sidebar_state="auto", menu_items=None)
