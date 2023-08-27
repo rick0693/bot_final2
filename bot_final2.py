907c1ab2bf21ebb0701e1532443b5f079b23f770
rick0693 <108191105+rick0693@users.noreply.github.com>
2023-08-27 14:28:07 -0300

    Update bot_final2.py

--- bot_final2.py
+++ bot_final2.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
