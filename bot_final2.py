b07b16997ea23edc071cd509f76475360a1c7d43
rick0693 <108191105+rick0693@users.noreply.github.com>
2023-08-27 14:24:14 -0300

    Update bot_final2.py

--- bot_final2.py
+++ bot_final2.py
@@ -1,3 +1,6 @@
+import streamlit as st
+import time
+
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
@@ -7,10 +10,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
 
-URL = ""
+URL = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
+XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
 TIMEOUT = 20
 
 st.title("Test Selenium")
+st.markdown("You should see some random Football match text below in about 21 seconds")
 
 firefoxOptions = Options()
 firefoxOptions.add_argument("--headless")
@@ -20,3 +25,17 @@ driver = webdriver.Firefox(
     service=service,
 )
 driver.get(URL)
+
+try:
+    WebDriverWait(driver, TIMEOUT).until(
+        EC.visibility_of_element_located((By.XPATH, XPATH,))
+    )
+
+except TimeoutException:
+    st.warning("Timed out waiting for page to load")
+    driver.quit()
+
+time.sleep(10)
+elements = driver.find_elements_by_xpath(XPATH)
+st.write([el.text for el in elements])
+driver.quit()
