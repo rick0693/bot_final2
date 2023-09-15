2a5c961794a8c3c8ee417b406b43649bcc3aac2f
rick0693 <108191105+rick0693@users.noreply.github.com>
2023-09-14 23:48:48 -0300

    Update bot_final2.py

--- bot_final2.py
+++ bot_final2.py
@@ -13,7 +13,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import pygame
 import json
 import os
 import subprocess
